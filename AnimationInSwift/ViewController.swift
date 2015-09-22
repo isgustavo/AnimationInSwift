560a1a49bdcd209ccf159711a3c8901c3784c73f
isgustavo <himynameisgustavo@gmail.com>
2015-09-22 14:19:21 -0300

    Initial Commit

---
+++ AnimationInSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationInSwift
+//
+//  Created by gustavo.freitas2 on 9/22/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
