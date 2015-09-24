7ef82565cc0cd016d7f06b625d3660203bb292ae
isgustavo <himynameisgustavo@gmail.com>
2015-09-24 11:02:21 -0300

    Animation in swift
    Study of animation.
    Reference material:
    http://mathewsanders.com/prototyping-iOS-iPhone-iPad-animations-in-swift
    / and http://mathewsanders.com/animations-in-swift-part-two/

--- AnimationInSwift/ViewController.swift
+++ AnimationInSwift/ViewController.swift
@@ -10,16 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOfFishSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for _ in 1...15 {
+            
+            let size: CGFloat = CGFloat(arc4random_uniform(40))+20
+            let yPosition: CGFloat = CGFloat( arc4random_uniform(200))+20
+            
+            let randomYOffset = CGFloat(arc4random_uniform(150))
+            let color = CGFloat(arc4random_uniform(3))
+            
+            
+            let fish = UIImageView()
+            
+            switch(color){
+            case 0:
+                fish.image = UIImage(named: "blue-fish.png")
+            case 1:
+                fish.image = UIImage(named: "red-fish.png")
+            default:
+                fish.image = UIImage(named: "orange-fish.png")
+            }
+            
+            
+            fish.frame = CGRectMake(0-size, yPosition, size, size)
+            self.view.addSubview(fish)
+            
+            let path = UIBezierPath()
+            path.moveToPoint(CGPoint(x: 16, y: 239 + randomYOffset))
+            path.addCurveToPoint(CGPoint(x: 301, y: 239 + randomYOffset),
+                controlPoint1: CGPoint(x: 136, y: 373 + randomYOffset),
+                controlPoint2: CGPoint(x: 178, y: 110 + randomYOffset))
+            
+            let anim = CAKeyframeAnimation(keyPath: "position")
+            anim.path = path.CGPath
+            anim.rotationMode = kCAAnimationRotateAuto
+            anim.repeatCount = Float.infinity
+            anim.duration = Double(arc4random_uniform(40)+30) / 10
+            anim.timeOffset = Double(arc4random_uniform(290))
+            
+            fish.layer.addAnimation(anim, forKey: "animate position along path")
+            
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
